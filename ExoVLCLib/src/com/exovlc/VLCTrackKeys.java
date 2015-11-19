87146a2e7f28b835c5e2406a5e281de3ac4c8db8
Tayeb Bouhadjar Yazid (tyazid) <tyazid@cisco.com>
2015-11-19 09:31:57 +0100

    Add studio project files

--- ExoVLCLib/src/com/exovlc/VLCTrackKeys.java
+++
@@ -1,9 +0,0 @@
-package com.exovlc;
-
-public interface VLCTrackKeys {
-
-	String VLC_SUBTITLE_TRACK_ECODING = "vlc/track.spu.enc";
-	String VLC_TRACK_DESCRIPTION = "vlc/track.desc";
-	String VLC_TRACK_ORIGINAL_CODEC = "vlc/track.org.codec";
-
-}
